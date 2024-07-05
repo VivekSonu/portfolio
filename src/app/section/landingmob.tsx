ac61ee83786e9a647dfeb26fca4575ccd4951cef
VivekSonu <viveksonu37@gmail.com>
2024-07-06 00:11:36 +0530

    first commit

--- src/app/section/landingmob.tsx
+++ src/app/section/landingmob.tsx
@@ -45,7 +45,7 @@ export default function LandingMob() {
                 </div>
             </div>
         </div> */}
-        <div className="h-[40rem] flex flex-col items-center justify-center overflow-hidden rounded-md ">
+        <div className="h-[40rem] flex flex-col items-center justify-center overflow-hidden rounded-md py-0 ">
           <Socials />
           <TypewriterEffects />
           <motion.h1
@@ -111,13 +111,12 @@ export default function LandingMob() {
               {/* <TextRevealCards/> */}
             </div>
           </motion.div>
-
-        </div>
-        <motion.div
+          <motion.div
             initial={{ opacity: 0, y: 50 }}
             transition={{ delay: 1.5, duration: 1.5 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: false }}
+            className="mt-14"
           >
             <div className="container_mouse text-[#565659]">
               <span className="mouse-btn border-black/5">
@@ -126,6 +125,9 @@ export default function LandingMob() {
               <span className="text-[10px]">Scroll Down</span>
             </div>
           </motion.div>
+
+        </div>
+
       </div>
     </div>
   );
